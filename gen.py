f516dcd642b5b952acd83b5038fe90e3741c8870
memset0 <memset0@outlook.com>
2019-02-02 13:32:03 +0800

    Update gen.py

---
+++ gen.py
@@ -0,0 +1,32 @@
+import os, yaml
+
+def mkdir(path):
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+def read_file(path):
+	mkdir(os.path.split(path)[0])
+	file = open(path, 'r+', encoding='utf8')
+	text = file.read()
+	file.close()
+	return text
+
+def write_file(text, path):
+	mkdir(os.path.split(path)[0])
+	file = open(path, 'w+', encoding='utf8')
+	file.write(text)
+	file.close()
+
+def load(text, data):
+	for key, value in data.items():
+		text = text.replace('{{ %s }}' % key, value)
+	return text
+
+if __name__ == '__main__':
+	for filename in os.listdir('src'):
+		text = read_file('src/%s' % filename)
+		data = yaml.load(text)
+		data['short_name'] = filename.split('.')[0]
+		for filename in os.listdir('themes/%s' % data['theme']):
+			text = read_file('themes/%s/%s' % (data['theme'], filename))
+			write_file(load(text, data), 'html/%s/%s' % (data['short_name'], filename))
