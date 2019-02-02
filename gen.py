8a69bc7e0abeeed630c32a27bdf39ed557f76f74
memset0 <memset0@outlook.com>
2019-02-02 13:43:44 +0800

    Update gen.py (每次修改后自动部署到对应仓库)

--- gen.py
+++ gen.py
@@ -22,6 +22,11 @@ def load(text, data):
 		text = text.replace('{{ %s }}' % key, value)
 	return text
 
+def depoly(name):
+	os.system("cd {path} && git add . && git commit -m 'update' && git push".format(
+		path = os.getcwd() + '/html/%s' % name
+	))
+
 if __name__ == '__main__':
 	for filename in os.listdir('src'):
 		text = read_file('src/%s' % filename)
@@ -30,3 +35,4 @@ if __name__ == '__main__':
 		for filename in os.listdir('themes/%s' % data['theme']):
 			text = read_file('themes/%s/%s' % (data['theme'], filename))
 			write_file(load(text, data), 'html/%s/%s' % (data['short_name'], filename))
+		depoly(data['short_name'])
