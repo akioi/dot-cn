eec9eadfdb1e9c5fd794a62f8bb468a38af57f2f
alpha1022 <alpha1022@alpha1022.me>
2019-02-10 20:04:26 +0800

    Try to fix gen.py

--- gen.py
+++ gen.py
@@ -139,7 +139,6 @@ if __name__ == '__main__':
 		data = yaml.load(text)
 		data['short_name'] = filename.split('.')[0]
 		print('Processing %s , Shortname : %s'%(filename,data['short_name']))
-		Cleanup()
 		for filename in os.listdir('themes/%s' % data['theme']):
 			text = read_file('themes/%s/%s' % (data['theme'], filename))
 			write_file(load(text, data), 'html/%s/%s' % (data['short_name'], filename))
