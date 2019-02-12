85463aeda77531d787ea9d575f05acdd07d4f8e6
alpha1022 <alpha1022@alpha1022.me>
2019-02-12 22:35:33 +0800

    Try to fix gen.py

--- gen.py
+++ gen.py
@@ -134,15 +134,15 @@ def Cleanup():
 
 if __name__ == '__main__':
 
-	mkdir(os.getcwd() + '/html/')
+	#mkdir(os.getcwd() + '/html/')
 	#LoginGithub()
 	for filename in os.listdir('src'):
 		text = read_file('src/%s' % filename)
 		data = yaml.load(text)
 		data['short_name'] = filename.split('.')[0]
 		print('Processing %s , Shortname : %s'%(filename,data['short_name']))
-		PrepareForRepo(data['short_name'])
 		Cleanup()
+		PrepareForRepo(data['short_name'])
 		for filename in os.listdir('themes/%s' % data['theme']):
 			text = read_file('themes/%s/%s' % (data['theme'], filename))
 			write_file(load(text, data), 'html/%s' % filename)
